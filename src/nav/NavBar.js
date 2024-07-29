0ef3d9ed68efe8a0bdd866c918611645fda95c31
ranjith50677 <kumarranjith50677@gmail.com>
2024-07-29 14:44:39 +0530

    small changes fixes

--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -17,37 +17,29 @@ import {
 } from "mdb-react-ui-kit";
 import Logo from "../assets/images/logo.png";
 import "../nav/navbar.css"
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
 export default function NavBar() {
   const [openBasic, setOpenBasic] = useState(false);
 
   return (
     <MDBNavbar expand="lg" className="nav-bar" style={{boxShadow:"none", height:"100px"}}>
-      {/* <MDBContainer fluid> */}
-        <MDBNavbarBrand href="/">
+      <div className="innerCon">
           <img
             className="nav-logo"
             // className="h-16 md:h-24 lg:h-28"
             alt="Beamtech logo"
             src={Logo}
           />
-        </MDBNavbarBrand>
-        <MDBNavbarBrand>
-
-          <MDBBtn className="submit-btn" style={{borderRadius:20}}>Get Started</MDBBtn>
-      </MDBNavbarBrand>
-      <MDBNavbarToggler
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          onClick={() => setOpenBasic(!openBasic)}
-        >
-          <MDBIcon icon="bars" fas />
-        </MDBNavbarToggler>
-        
+          <div className="buttonCon">
+     
+        </div>
           {/* <MDBIcon icon="times" fas /> */}
-
-        <MDBCollapse navbar open={openBasic}>
-          <MDBNavbarNav className="mr-auto mb-2 mb-lg-0">
+        {/* <MDBCollapse navbar open={openBasic}> */}
+        <div className="nav-link-connects">
+          <MDBNavbarNav className="forNewNav mr-auto mb-2 mb-lg-0" >
           
             <MDBNavbarItem>
               <MDBNavbarLink active aria-current="page" href="#">
@@ -71,7 +63,7 @@ export default function NavBar() {
 
             {/* <MDBNavbarItem>
               <MDBNavbarLink
-                disabled
+              disabled
                 href="#"
                 tabIndex={-1}
                 aria-disabled="true"
@@ -80,11 +72,54 @@ export default function NavBar() {
               </MDBNavbarLink>
             </MDBNavbarItem> */}
           </MDBNavbarNav>
-
+        </div>
           
-        </MDBCollapse>
+
+        <div style={{display:"flex", justifyContent:"center", position:"relative", zIndex:10,right:"10px"}} className="flex justify-center py-6 relative z-10">
+                    <Link
+                        // to="/package"
+                        // state={{ packageDetails }}
+                        style={{borderRadius:20, textDecoration:"none",backgroundColor:"#212529"}}
+                        className="cursor-pointer px-4 py-2 text-white submit-btn rounded-full text-lg"
+                    >
+                        <span className="flex items-center">Speak With Us</span>
+                    </Link>
+                </div>
+    
+        <MDBNavbarToggler
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setOpenBasic(!openBasic)}
+        >
+          <MDBIcon icon="bars" fas />
+        </MDBNavbarToggler>
+        
+        </div >
+        <MDBCollapse navbar open={openBasic}>
+        <MDBNavbarNav className="forNewNav mr-auto mb-2 mb-lg-0" >
+          
+          <MDBNavbarItem>
+            <MDBNavbarLink active aria-current="page" href="#">
+              About
+            </MDBNavbarLink>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            <MDBNavbarLink href="/about">Solutions</MDBNavbarLink>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            <MDBNavbarLink href="/process">Process</MDBNavbarLink>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            <MDBNavbarLink href="/work">Work</MDBNavbarLink>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            <MDBNavbarLink href="/contact">Contact</MDBNavbarLink>
+          </MDBNavbarItem>
+        </MDBNavbarNav>
       {/* </MDBContainer> */}
       
+  </MDBCollapse>
     </MDBNavbar>
   );
 }
